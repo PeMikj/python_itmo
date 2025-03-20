760df9b3d7ef0a0d17d71d04d309329979faecf4
pm <pedromik@mail.ru>
2025-03-21 00:38:24 +0400

    hw_2

---
+++ hw_2/generate_table_example.py
@@ -0,0 +1,26 @@
+import os
+from latex_gen.latex_gen import generate_latex_table, generate_latex_image, generate_full_document, compile_pdf
+
+artifacts_dir = "artifacts"
+os.makedirs(artifacts_dir, exist_ok=True)
+
+table_data = [
+    ["FROM", "TO", "PRICE"],
+    ["A", "B", 17],
+    ["here", "there", 19],
+    ["C", "D", 22]
+]
+
+table_code = generate_latex_table(table_data)
+image_code = generate_latex_image("example_image.jpg")
+
+full_content = table_code + "\n" + image_code
+document = generate_full_document(full_content)
+
+tex_output_path = os.path.join(artifacts_dir, "generated_document.tex")
+with open(tex_output_path, "w", encoding="utf-8") as tex_file:
+    tex_file.write(document)
+
+pdf_output_path = os.path.join(artifacts_dir, "generated_artifact.pdf")
+compile_pdf(document, pdf_output_path)
+print(f"PDF с таблицей и картинкой сгенерирован: {pdf_output_path}")
